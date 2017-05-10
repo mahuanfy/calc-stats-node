6d461d8d8312e67c0d38ee718f2a3716b2cdba50
Seaborn Lee <seabornlee.cn@gmail.com>
2017-05-10 18:30:33 +0800

    add skeleton code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,18 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
+    console.log(`o) 最小值 = ${sequence.minimum()}
+// Write your code here
+`)
 };
+
+class Sequence {
+  constructor(input) {
+    // Write your code here
+  }
+
+  minimum() {
+    // Write your code here
+  }
+
+  // Write your code here
+}
