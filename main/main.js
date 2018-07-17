b9665c1b10107bf4524860a77a0dd02ee4d05046
mahuanfy <mahuanfy@foxmail.com>
2018-07-17 11:53:56 +0800

    Complete the test

--- main/main.js
+++ main/main.js
@@ -1,18 +1,30 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.arrayLength()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+    this.input = input
   }
-
   minimum() {
-    // Write your code here
+   return Math.min.apply(Math,this.input)
   }
-
-  // Write your code here
+  maximum(){
+    return Math.max.apply(Math,this.input)
+  }
+  arrayLength(){
+    return this.input.length
+  }
+  average(){
+    var sum=0;
+    for (var i = 0; i < this.input.length; i++) {
+        sum+=this.input[i];
+    };
+    var avg=(sum/this.input.length).toFixed(2);
+    return avg
+  }
 }
